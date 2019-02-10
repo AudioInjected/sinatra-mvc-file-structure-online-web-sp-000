0476606a64c2d4ac216e30c6da46baba2625c5a3
Rafael Batista <rafaelb24@yahoo.com>
2019-02-10 18:41:42 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,14 @@
 class Dog 
+  @@all = []
   def initialize(name, breed, age) 
     @name = name 
     @breed = breed 
     @age = age 
+    @@all << self
   end 
+  
+  def self.all 
+    @@all
+  end 
+  
 end 
